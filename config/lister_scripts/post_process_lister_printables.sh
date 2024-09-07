b6d2a77991813a57abcc20a76735ca3a526e4bc7
Jason Schoeman <jason@schoeman.me>
2024-09-07 14:37:27 +0200

    Improved for first attempt to process thumbnails better

---
+++ config/lister_scripts/post_process_lister_printables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Make this script executable
+chmod +x "$0"
+
+# Rest of your script goes here
+REPO_PATH="~/printer_data/gcodes/lister_printables"
+
+# Create .thumbs directories
+find "$REPO_PATH" -type d | while read -r dir; do
+    mkdir -p "$dir/.thumbs"
+    chmod 755 "$dir/.thumbs"
+done
