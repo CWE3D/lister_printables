22e36efefd3d29386c0887f6219d4f68c5a0a237
Jason Schoeman <jason@schoeman.me>
2024-09-09 08:20:27 +0200

    Added thumbs.

--- scripts/post_processing.sh
+++ scripts/post_processing.sh
@@ -11,3 +11,10 @@ find "$REPO_PATH" -type d | while read -r dir; do
     mkdir -p "$dir/.thumbs"
     chmod 755 "$dir/.thumbs"
 done
+
+# Extract thumbnails
+find "$REPO_PATH" -name "*.gcode" | while read -r file; do
+    dir=$(dirname "$file")
+    filename=$(basename "$file")
+    extract-gcode-thumbnail "$file" -o "$dir/.thumbs/$filename.png"
+done
